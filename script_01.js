c9a27400e102857bff64826c6047e0ce339825ce
andante-maestoso <h.plottke@mail.de>
2021-02-12 13:26:23 +0100

    zurück

--- script_01.js
+++ script_01.js
@@ -1,35 +1,71 @@
-//Programm Sortieren:
 
-let zahlen, tausch, x, länge;
+/*** 1.0 Arrays */
+/*
+let arr;
 
-ausgabe(sortieren(eingabe(zahlen)));
+arr = new Array(); // Konstruktor
+arr = [];  //Literal
+arr = [2,6,7,12];
+arr = [true, false];
+arr = ["Ich", "bin", "Holger"];
 
-function eingabe (zahlen) {
-    länge = parseInt(prompt("Bitte die Anzahl der Zahlen eingeben:"))
-    zahlen = new Array (länge);
-    for (let i = 0; i < länge; i++) {
-        x=1+i;
-        zahlen[i] = parseInt(prompt("Bitte die "+ x +". Zahl eingeben:"))
-    }
-    ausgabe (zahlen);
-    return (zahlen);
-    
+
+ausgabe(arr);
+ausgabe(arr[0]);
+ausgabe(arr[2]);
+ausgabe(arr.length);
+*/
+
+
+//ausgabe(getSentence("Ich","bin","Holger", "Plottke"));    // Test
+//arr = ["Ich","bin","Holger", "Plottke"];
+//ausgabe(getSentence(arr));
+function getSentence(arr) {
+    let gap = " ";
+    let punct = ".";
+   
+        
+     
+    //let str = arr[0] + gap + arr[1] + gap + arr[2] + gap + arr[3] + punct;
+   // return str;
 }
 
-function sortieren(zahlen) {
-    for (let i = 0; i < zahlen.length; i++) {
-        
-        for (let j = 0; j < zahlen.length; j++) {
-            
-            if (zahlen[j] > zahlen[1+j] )
-            {tausch = zahlen[j];
-            zahlen[j] = zahlen[1+j];
-            zahlen[1+j] = tausch;}
-        }
-    }
-    return zahlen;
-}
-    
+
+
+// Modul: Ausgabe in Konsole | Test:
+//ausgabe("hi");
 function ausgabe(outputStr) {
     console.log(outputStr);
 }
+
+
+
+let arr = ["Ich","bin","Holger", "Plottke"];
+let str = "";
+let gap = " ";
+let punct = ".";
+ausgabe(satzMachen2(arr));
+    function satzMachen(arr){
+        for (let i = 0; i < arr.length; i++) {
+            if (i != (arr.length-1)) {
+                str += arr[i] + gap;
+            } else {
+                str += arr[i] + punct;
+            }
+        }           
+    return str;
+    }
+
+
+
+
+
+
+    function satzMachen2(arr){
+        for (let i = 0; i < arr.length-1; i++) {
+            str += arr[i] + gap;
+            } 
+            str += arr[arr.length-1] + punct;       
+    return str;
+    }
+
